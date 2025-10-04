92373a88a0b124d2f5b7c9fa8d3e294600aa1fa3
Blizmofa <blizmofa@gmail.com>
2025-10-04 17:22:36 +0300

    Added constants and util method to parse int64

--- routes/register.go
+++ routes/register.go
@@ -2,44 +2,52 @@ package routes
 
 import (
 	"net/http"
-	"strconv"
 
+	"example.com/go-rest/constants"
 	"example.com/go-rest/models"
+	"example.com/go-rest/utils"
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	msgRegisterEvent      = "Could not register event."
+	msgCancelRegistration = "Could not cancel event registration."
+	msgRegisteredOK       = "Registered Event!"
+	msgCancelledOK        = "Cancelled Registration!"
+)
+
 func registerForEvent(context *gin.Context) {
-	userId := context.GetInt64("userId")
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	userId := context.GetInt64(constants.ContextUserID)
+	eventId, err := utils.ParseInt64(context.Param(constants.ID))
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
+		context.JSON(http.StatusBadRequest, gin.H{"message": msgParseEventID})
 		return
 	}
 
 	event, err := models.GetEventById(eventId)
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": msgFetchEvent})
 		return
 	}
 
 	err = event.Register(userId)
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register event."})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": msgRegisterEvent})
 		return
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"message": "Registered Event!"})
+	context.JSON(http.StatusCreated, gin.H{"message": msgRegisterEvent})
 }
 
 func cancelRegistration(context *gin.Context) {
-	userId := context.GetInt64("userId")
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	userId := context.GetInt64(constants.ContextUserID)
+	eventId, err := utils.ParseInt64(context.Param(constants.ID))
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
+		context.JSON(http.StatusBadRequest, gin.H{"message": msgParseEventID})
 		return
 	}
 
@@ -49,10 +57,10 @@ func cancelRegistration(context *gin.Context) {
 	err = event.CancelRegistration(userId)
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel event registration."})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": msgParseEventID})
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Cancelled Registration!"})
+	context.JSON(http.StatusOK, gin.H{"message": msgCancelledOK})
 
 }
