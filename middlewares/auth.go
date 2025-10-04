278aee69e9f9f0c2f4f05d89b11f0be92a794fff
Blizmofa <blizmofa@gmail.com>
2025-10-04 17:28:47 +0300

    Added constants

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 	"strings"
 
+	"example.com/go-rest/constants"
 	"example.com/go-rest/utils"
 	"github.com/gin-gonic/gin"
 )
+
+const msgNotAuthorized = "Not authorized."
 
 func Authenticate(context *gin.Context) {
 	auth := context.Request.Header.Get("Authorization")
 
 	if auth == "" {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": msgNotAuthorized})
 		return
 	}
 
@@ -23,11 +26,11 @@ func Authenticate(context *gin.Context) {
 	userId, err := utils.VerifyToken(token)
 
 	if err != nil {
-		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": msgNotAuthorized})
 		return
 	}
 
-	context.Set("userId", userId)
+	context.Set(constants.ContextUserID, userId)
 
 	context.Next()
 }
