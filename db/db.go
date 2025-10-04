32e3867916520f6275736e647e08cf2a17244f9c
Blizmofa <blizmofa@gmail.com>
2025-10-04 16:23:19 +0300

    Refactored create table to be a generic method

--- db/db.go
+++ db/db.go
@@ -2,41 +2,23 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
 	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
 
-func InitDB() {
-	var err error
-	DB, err = sql.Open("sqlite", "api.db")
-
-	if err != nil {
-		panic("Could not connect to database: " + err.Error())
-	}
-
-	DB.SetMaxOpenConns(10)
-	DB.SetMaxIdleConns(5)
-
-	createTables()
-}
-
-func createTables() {
-	createUsersTable := `
+// Table creation SQL constants
+const (
+	createUsersTable = `
 	CREATE TABLE IF NOT EXISTS users (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	email TEXT NOT NULL UNIQUE,
-	password TEXT NOT NULL
-	)
-	`
-	_, err := DB.Exec(createUsersTable)
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	);`
 
-	if err != nil {
-		panic("Could not create users table: " + err.Error())
-	}
-
-	createEventTables := `
+	createEventsTable = `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
@@ -45,28 +27,38 @@ func createTables() {
 		dateTime DATETIME NOT NULL,
 		user_id INTEGER,
 		FOREIGN KEY(user_id) REFERENCES users(id)
-	)
-	`
+	);`
 
-	_, err = DB.Exec(createEventTables)
+	createRegistrationsTable = `
+	CREATE TABLE IF NOT EXISTS registrations (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id INTEGER,
+		FOREIGN KEY(event_id) REFERENCES events(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+)
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite", "api.db")
 
 	if err != nil {
-		panic("Could not create events table: " + err.Error())
+		log.Fatalf("Could not connect to database: %v", err.Error())
 	}
 
-	createRegistrationTable := `
-	CREATE TABLE IF NOT EXISTS registrations (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	event_id INTEGER,
-	user_id INTEGER,
-	FOREIGN KEY(event_id) REFERENCES events(id)
-	FOREIGN KEY(user_id) REFERENCES users(id)
-	)
-	`
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
 
-	_, err = DB.Exec(createRegistrationTable)
+	createTable(createUsersTable)
+	createTable(createEventsTable)
+	createTable(createRegistrationsTable)
+}
+
+func createTable(tableQuery string) {
+	_, err := DB.Exec(tableQuery)
 
 	if err != nil {
-		panic("Could not create registrations table: " + err.Error())
+		log.Fatalf("Could not create table: %v", err.Error())
 	}
 }
